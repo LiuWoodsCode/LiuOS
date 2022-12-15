43262825b46dd8151bffc90d154c3f0d633da40d
Liu Woods <68132166+LiuWoodsCode@users.noreply.github.com>
2022-12-15 11:48:10 -0500

    Add sample credentials
    Add sample credentials. Username is "username", password is "password", both without quotes.

--- cred.py
+++ cred.py
@@ -1,4 +1,4 @@
 ## Credentials
-loginname = ""
+loginname = "username"
 ## Hash of your password V
-loginpass = ""
+loginpass = "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86"
