3b6a8714c729e6aa801c36242745f151acf1e10e
LiuWoodsCode <68132166+LiuWoodsCode@users.noreply.github.com>
2022-12-14 20:19:27 -0500

    Update core.py

--- core.py
+++ core.py
@@ -10,26 +10,29 @@ FORMAT = '%(levelname)s | TIME - %(asctime)s | PROCESS - %(processName)s %(proce
 logging.basicConfig(filename='LiuOS.log', encoding='utf-8', level=logging.DEBUG, format=FORMAT)
 # Counter
 attemps = 0
-def actualsys() :
-    print("Logged in")
-# Authentication system
-while attemps < 3:
-    username = input(lang.ENTER_USERNAME_LOGIN)
-    logging.debug('Entered username')
-    password = getpass.getpass(lang.ENTER_PASSWD_LOGIN)
-    logging.debug('Entered password')
-    bytehash = hashlib.sha512(password.encode())
-    pwdreshash = bytehash.hexdigest()
-    logging.debug('Generated hash of password')
-    if environ.get('GITHUB_ACTIONS') != "false":
+if environ.get('GITHUB_ACTIONS') != "false":
         actualsys()
-    elif username == cred.loginname and pwdreshash == cred.loginpass:
-        print(lang.SUCCESSFUL_LOGIN)
-        logging.debug('Correct login credentials, logged in')
-        actualsys()
-    else:
-        print(lang.INCORRECT_LOGIN)
-        logging.error("Incorrect login credentials")
-        attemps += 1
-        continue
+else:
+    def actualsys() :
+        print("Logged In")
+ # Authentication system
+    while attemps < 3:
+        username = input(lang.ENTER_USERNAME_LOGIN)
+        logging.debug('Entered username')
+        password = getpass.getpass(lang.ENTER_PASSWD_LOGIN)
+        logging.debug('Entered password')
+        bytehash = hashlib.sha512(password.encode())
+        pwdreshash = bytehash.hexdigest()
+        logging.debug('Generated hash of password')
+        if environ.get('GITHUB_ACTIONS') != "false":
+            actualsys()
+        elif username == cred.loginname and pwdreshash == cred.loginpass:
+            print(lang.SUCCESSFUL_LOGIN)
+            logging.debug('Correct login credentials, logged in')
+            actualsys()
+        else:
+            print(lang.INCORRECT_LOGIN)
+            logging.error("Incorrect login credentials")
+            attemps += 1
+            continue
 print("")
