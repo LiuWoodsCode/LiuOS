5ec1991f03541a54fae1580d920d911fe7e75528
LiuWoodsCode <68132166+LiuWoodsCode@users.noreply.github.com>
2022-12-14 20:00:18 -0500

    Added GHA bypass

--- core.py
+++ core.py
@@ -4,11 +4,14 @@ import getpass
 import lang
 import cred
 import logging
+import os
 # Log formatting
 FORMAT = '%(levelname)s | TIME - %(asctime)s | PROCESS - %(processName)s %(process)d | MSG - %(message)s'
 logging.basicConfig(filename='LiuOS.log', encoding='utf-8', level=logging.DEBUG, format=FORMAT)
 # Counter
 attemps = 0
+def actualsys() :
+    print("Logged in")
 # Authentication system
 while attemps < 3:
     username = input(lang.ENTER_USERNAME_LOGIN)
@@ -18,11 +21,12 @@ while attemps < 3:
     bytehash = hashlib.sha512(password.encode())
     pwdreshash = bytehash.hexdigest()
     logging.debug('Generated hash of password')
-    
-    if username == cred.loginname and pwdreshash == cred.loginpass:
+    if "runningactions" in os.environ:
+        actualsys()
+    elif username == cred.loginname and pwdreshash == cred.loginpass:
         print(lang.SUCCESSFUL_LOGIN)
         logging.debug('Correct login credentials, logged in')
-        break
+        actualsys()
     else:
         print(lang.INCORRECT_LOGIN)
         logging.error("Incorrect login credentials")
