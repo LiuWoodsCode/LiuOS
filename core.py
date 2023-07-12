55ab58464f05c7ce209bbbf5a629902c9bd49c51
LiuWoodsCode <liuwoods.scpfan@outlook.com>
2023-07-12 13:09:39 -0400

    Fix cred

--- core.py
+++ core.py
@@ -22,6 +22,16 @@ import cred
 logging.debug("Imported cred.py")
 import os
 logging.debug("Imported os")
+def rename_file_if_not_exists(filename, new_filename):
+    if not os.path.exists(filename):
+        if os.path.exists(new_filename):
+            return True
+        else:
+            os.rename(new_filename, filename)
+            print(f"Created default cred file.")
+    else:
+        return True
+rename_file_if_not_exists("defaultcred.py", "cred.py")
 import sys
 logging.debug("Imported sys")
 import cmd
