d751b419d193e04a7da538c226b780e0370a88c6
LiuWoodsCode <liuwoods.scpfan@outlook.com>
2023-07-21 11:40:28 -0400

    Improved setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-import os
-def rename_file_if_not_exists(filename, new_filename):
-    if not os.path.exists(filename):
-        if os.path.exists(new_filename):
-            return True
-        else:
-            os.rename(new_filename, filename)
-            print(f"Created default cred file.")
+import platform
+import subprocess
+
+def get_os_specific_command():
+    current_os = platform.system()
+    if current_os == "Windows":
+        return "copy defaultcred.py cred.py"
+    elif current_os == "Darwin" or current_os == "Linux":
+        return "cp -- \"defaultcred.py\" \"cred.py\""
     else:
-        return True
-rename_file_if_not_exists("defaultcred.py", "cred.py")
+        raise OSError("LiuOS is incompatible with your low-level OS.")
+
+def run_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
+        print(output)
+    except subprocess.CalledProcessError as e:
+        print("Error executing the command:", e)
+
+if __name__ == "__main__":
+    current_directory_command = get_os_specific_command()
+    run_command(current_directory_command)
